91ae1f062698fbb76dbfe21ed47dc4ac7d0c59ab
SaiAbinesh <s.natarajan3@nuigalway.ie>
2018-11-12 12:46:16 +0000

    Safety backup 1

--- generate_coco_json.py
+++ generate_coco_json.py
@@ -1,6 +1,7 @@
 import datetime
 import json 
-global date = str(datetime.datetime.now())
+global date
+date = str(datetime.datetime.now())
 print(date)
 
 #master_json_dict = {} #The final dictionary that will be dumped into the json
@@ -19,19 +20,19 @@ licenses_list = [{"url": "http:\/\/creativecommons.org\/licenses\/by-nc-sa\/2.0\
 #Creating the master json dictionary using the prepopulated fields above
 master_json_dict = {"info": info__dict, "images": [], "annotations": [], "licenses": licenses_list, "categories": categories_list  }
 
-print("categories list for id 10: ", categories_list[9])
-print(master_json_dict)
+# print("categories list for id 10: ", categories_list[9])
+# print(master_json_dict)
 
 # The following function outputs a python dictionary that can be written into a file as a json. Right now bounding boxes are not written
 def create_annotations(category_id_list, folder_path, master_json_dict, path_binary_list_of_flags):
 	global date
 	# Writing the basic image info for each image
-	for image_number in range(1,10):
+	for image_number in range(400,470):
 		image_temp = {
 		"id": image_number,
 		"width": 2000,
 		"height": 2000,
-		"file_name": folder_path+"/raw_"+str(image_number)+".jpg",
+		"file_name": folder_path+"/image_"+str(image_number)+"_raw.png",
 		"license": 1,
 		"flickr_url": "none",
 		"coco_url": "none",
@@ -45,12 +46,12 @@ def create_annotations(category_id_list, folder_path, master_json_dict, path_bin
 		current_category_id = category_id_list[i][0] #The first element in each list is the category id
 		current_category_name = category_id_list[i][1] #The second element is the category name
 		number_of_instances_in_category = category_id_list[i][2] #The third element is the number of instances in that category
-		for instance_number in (1,len(number_of_instances_in_category)+1):
+		for instance_number in range(1,number_of_instances_in_category+1):
 			#Reading the text file containing the binary flags of 1s and 0s and storing it into a list. The text file is of the format ex. "list_gas cylinder_5"
-			binary_flags_object_present = (path_binary_list_of_flags+"/list_"+current_category_name+"_"+str(instance_number)+".txt").read().splitlines())
+			binary_flags_object_present = open(path_binary_list_of_flags+"/list_"+current_category_name+"_"+str(instance_number)+".txt").read().splitlines()
 			#Converting the elements of the list into int
 			binary_flags_object_present =  [int(item) for item in binary_flags_object_present]
-			for image_number in range(1,10):
+			for image_number in range(400,470):
 				if binary_flags_object_present[image_number-1]: # image_number-1 since image_number is indexed from 1, and binary flags are python indexed from 0
 					annotations_temp = {
 					"id": annotation_count ,
@@ -68,10 +69,12 @@ def create_annotations(category_id_list, folder_path, master_json_dict, path_bin
 # print(master_json_dict["annotations"])
 folder_path = "D:/AirSim/New/Images/Images_master"
 #list containing lists of [[cat_id, num_instances],..] 10 - gas cylinder, 7- train, 8 - truck
-category_id_list = [[10,5],[7,1],[8,3]]
-path_binary_list_of_flags = "D:\AirSim\New\AirSim\PythonClient\computer_vision"
-file_path_for_json = "D:/AirSim/New/Images/coco/annotations/instances_train2014.json"
+category_id_list = [[10,"gas cylinder", 5],[7, "train", 1],[8,"truck",3]]
+path_binary_list_of_flags = "D:/AirSim/New/AirSim/PythonClient/computer_vision"
+create_annotations(category_id_list, folder_path, master_json_dict, path_binary_list_of_flags)
 
+file_path_for_json = "D:/AirSim/New/Images/coco/annotations/test_instances_500-530_images_train2014.json"
 with open(file_path_for_json, 'w') as data_file:
 	json.dump(master_json_dict,data_file)
 
+print("Done")	
